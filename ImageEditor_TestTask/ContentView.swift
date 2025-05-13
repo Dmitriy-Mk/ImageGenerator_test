a7fc6fdb5739b4c5bd4b8d3d2fe17beff90bcad3
Dmitriy_Mk <Dmitriy.mkrtumov@gmail.com>
2025-05-13 22:45:27 +0400

    Initial Commit

---
+++ ImageEditor_TestTask/ContentView.swift
@@ -0,0 +1,25 @@
+//
+// ContentView.swift
+// ImageEditor_TestTask
+//
+// Created by Dmitriy Mk on 13.05.25.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageEditor_TestTask/ImageEditor_TestTaskApp.swift
@@ -0,0 +1,18 @@
+//
+// ImageEditor_TestTaskApp.swift
+// ImageEditor_TestTask
+//
+// Created by Dmitriy Mk on 13.05.25.
+//
+
+
+import SwiftUI
+
+@main
+struct ImageEditor_TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
