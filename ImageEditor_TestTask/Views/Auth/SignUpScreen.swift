7a317f05d59f452c1c4761529ea3e877f1b8a0ca
Dmitriy_Mk <Dmitriy.mkrtumov@gmail.com>
2025-05-14 23:26:19 +0400

    chore: Update singup screen layout

--- ImageEditor_TestTask/Views/Auth/SignUpScreen.swift
+++ ImageEditor_TestTask/Views/Auth/SignUpScreen.swift
@@ -12,34 +12,49 @@ struct SignUpScreen: View {
     
     @State private var email: String = ""
     @State private var password: String = ""
+    @State private var confirmPassword: String = ""
+    
+    private var isSignUpButtonDisabled: Bool {
+        email.isEmpty || password.isEmpty || confirmPassword.isEmpty || password != confirmPassword
+    }
+    private enum Constants {
+        static let textFieldVerticalPadding: CGFloat = 20.0
+    }
     
     var body: some View {
-        VStack {
+        VStack(spacing: 12) {
             Text("Create your profile")
                 .font(.largeTitle)
                 .bold()
+                .padding(.bottom, 24)
             
             TextField("Insert Email", text: $email)
                 .textInputAutocapitalization(.never)
-                .padding()
+                .autocorrectionDisabled()
                 .textFieldStyle(.roundedBorder)
+                .padding([.leading, .trailing], Constants.textFieldVerticalPadding)
             
             SecureField("Insert Password", text: $password)
                 .textInputAutocapitalization(.never)
-                .padding()
+                .autocorrectionDisabled()
                 .textFieldStyle(.roundedBorder)
+                .padding([.leading, .trailing], Constants.textFieldVerticalPadding)
             
-            SecureField("Confirm Password", text: $password)
+            SecureField("Confirm Password", text: $confirmPassword)
                 .textInputAutocapitalization(.never)
-                .padding()
+                .autocorrectionDisabled()
                 .textFieldStyle(.roundedBorder)
+                .padding([.leading, .trailing], Constants.textFieldVerticalPadding)
             
             Button("Sign up") {
-               
+                print("Proceed to sign up")
             }
             .buttonStyle(.borderedProminent)
+            .padding(.top, 16)
+            .disabled(isSignUpButtonDisabled)
+            .opacity(isSignUpButtonDisabled ? 0.5 : 1.0)
         }
-        .padding()
+        .padding(.horizontal)
     }
 }
 
