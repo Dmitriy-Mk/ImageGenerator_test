6f007b9c197cc4d614c99cbf8b8376f894d9645e
Dmitriy_Mk <Dmitriy.mkrtumov@gmail.com>
2025-05-15 17:13:47 +0400

    init

--- ImageEditor_TestTask/ImageEditorAppEntryPoint.swift
+++ ImageEditor_TestTask/ImageEditorAppEntryPoint.swift
@@ -10,6 +10,9 @@ import SwiftUI
 
 @main
 struct ImageEditor_TestTaskApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
